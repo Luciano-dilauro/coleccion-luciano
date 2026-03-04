ae1cd88280636648878366dd3fdb63b7b12ee622
Luciano-dilauro <luciano_dilauro@hotmail.com>
2026-03-04 19:25:54 -0300

    Update app.js

--- v4/app.js
+++ v4/app.js
@@ -1,1 +1,24 @@
+(() => {
 
+  const $ = (id) => document.getElementById(id);
+
+  function init() {
+
+    const status = $("status");
+    const btn = $("btnTest");
+
+    if (status) {
+      status.textContent = "v4 lista ✅";
+    }
+
+    if (btn) {
+      btn.addEventListener("click", () => {
+        alert("Botón v4 funcionando 🚀");
+      });
+    }
+
+  }
+
+  document.addEventListener("DOMContentLoaded", init);
+
+})();
